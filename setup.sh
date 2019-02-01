c9807c1fc6b1337a4d1c91493828ca07a93ea553
danacr <dan@kryha.io>
2019-02-01 14:24:42 +0100

    fixed validator

--- setup.sh
+++ setup.sh
@@ -3,26 +3,23 @@ docker-compose pull member0
 docker-compose up -d member0 member1 member2
 sleep 10
 
-curl --data '{"jsonrpc":"2.0","method":"parity_addReservedPeer","params":["enode://RESULT"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8541
-
-
-
 acc0=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["alicepwd", "alicepwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8545|jq .result)
 acc1=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["bobpwd", "bobpwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8544|jq .result)
 acc2=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["charliepwd", "charliepwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8543|jq .result)
 
-sed  -i  s,accountx,$acc0,g parity/config/alice.toml
-sed  -i  s,accountx,$acc1,g parity/config/bob.toml
-sed  -i  s,accountx,$acc2,g parity/config/charlie.toml
+sed  -i '' -e  s,accountx,$acc0,g parity/config/alice.toml
+sed  -i '' -e  s,accountx,$acc1,g parity/config/bob.toml
+sed  -i '' -e  s,accountx,$acc2,g parity/config/charlie.toml
 
 
-sed -i  "/signer/s/^#//g" parity/config/alice.toml
-sed -i  "/account/s/^#//g" parity/config/alice.toml
-sed -i  "/unlock/s/^#//g" parity/config/alice.toml
-sed -i  "/signer/s/^#//g" parity/config/bob.toml
-sed -i  "/account/s/^#//g" parity/config/bob.toml
-sed -i  "/unlock/s/^#//g" parity/config/bob.toml
-sed -i  "/signer/s/^#//g" parity/config/charlie.toml
-sed -i  "/account/s/^#//g" parity/config/charlie.toml
-sed -i  "/unlock/s/^#//g" parity/config/charlie.toml
+sed -i '' -e "/validator/s/^#//g" parity/config/alice.toml
+sed -i '' -e "/account/s/^#//g" parity/config/alice.toml
+sed -i '' -e "/unlock/s/^#//g" parity/config/alice.toml
+sed -i '' -e "/validator/s/^#//g" parity/config/bob.toml
+sed -i '' -e "/account/s/^#//g" parity/config/bob.toml
+sed -i '' -e "/unlock/s/^#//g" parity/config/bob.toml
+sed -i '' -e "/validator/s/^#//g" parity/config/charlie.toml
+sed -i '' -e "/account/s/^#//g" parity/config/charlie.toml
+sed -i '' -e "/unlock/s/^#//g" parity/config/charlie.toml
+
 docker kill $(docker ps -q)
