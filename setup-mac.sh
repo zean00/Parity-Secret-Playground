90a655b60126727f1209ac313bf22d0ab814b28f
danacr <dan@kryha.io>
2019-02-01 12:33:58 +0100

    added mac

--- setup-mac.sh
+++ setup-mac.sh
@@ -11,18 +11,19 @@ acc0=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","par
 acc1=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["bobpwd", "bobpwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8544|jq .result)
 acc2=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["charliepwd", "charliepwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8543|jq .result)
 
-sed  -i  s,accountx,$acc0,g parity/config/alice.toml
-sed  -i  s,accountx,$acc1,g parity/config/bob.toml
-sed  -i  s,accountx,$acc2,g parity/config/charlie.toml
+sed  -i '' -e  s,accountx,$acc0,g parity/config/alice.toml
+sed  -i '' -e  s,accountx,$acc1,g parity/config/bob.toml
+sed  -i '' -e  s,accountx,$acc2,g parity/config/charlie.toml
 
 
-sed -i  "/signer/s/^#//g" parity/config/alice.toml
-sed -i  "/account/s/^#//g" parity/config/alice.toml
-sed -i  "/unlock/s/^#//g" parity/config/alice.toml
-sed -i  "/signer/s/^#//g" parity/config/bob.toml
-sed -i  "/account/s/^#//g" parity/config/bob.toml
-sed -i  "/unlock/s/^#//g" parity/config/bob.toml
-sed -i  "/signer/s/^#//g" parity/config/charlie.toml
-sed -i  "/account/s/^#//g" parity/config/charlie.toml
-sed -i  "/unlock/s/^#//g" parity/config/charlie.toml
+sed -i '' -e "/signer/s/^#//g" parity/config/alice.toml
+sed -i '' -e "/account/s/^#//g" parity/config/alice.toml
+sed -i '' -e "/unlock/s/^#//g" parity/config/alice.toml
+sed -i '' -e "/signer/s/^#//g" parity/config/bob.toml
+sed -i '' -e "/account/s/^#//g" parity/config/bob.toml
+sed -i '' -e "/unlock/s/^#//g" parity/config/bob.toml
+sed -i '' -e "/signer/s/^#//g" parity/config/charlie.toml
+sed -i '' -e "/account/s/^#//g" parity/config/charlie.toml
+sed -i '' -e "/unlock/s/^#//g" parity/config/charlie.toml
+
 docker kill $(docker ps -q)
