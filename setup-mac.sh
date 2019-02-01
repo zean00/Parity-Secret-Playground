a2bfc6943f662284b2a9bac7522b8d12a2e7cffb
danacr <dan@kryha.io>
2019-02-01 12:58:36 +0100

    added parity account method

--- setup-mac.sh
+++ setup-mac.sh
@@ -3,10 +3,6 @@ docker-compose pull member0
 docker-compose up -d member0 member1 member2
 sleep 10
 
-curl --data '{"jsonrpc":"2.0","method":"parity_addReservedPeer","params":["enode://RESULT"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8541
-
-
-
 acc0=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["alicepwd", "alicepwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8545|jq .result)
 acc1=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["bobpwd", "bobpwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8544|jq .result)
 acc2=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["charliepwd", "charliepwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8543|jq .result)
