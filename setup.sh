50801e25b660079976b44ecc6f3e5c4c5f6e2bde
danacr <dan@kryha.io>
2019-02-05 17:18:57 +0100

    working bash

--- setup.sh
+++ setup.sh
@@ -7,9 +7,13 @@ alice=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","pa
 bob=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["bobpwd", "bobpwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8544|jq .result)
 charlie=$(curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["charliepwd", "charliepwd"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8543|jq .result)
 
+sed -i '' -e  s,accountx,$alice,g parity/config/alice.toml
+sed -i '' -e  s,accountx,$alice,g parity/config/bob.toml
+sed -i '' -e  s,accountx,$alice,g parity/config/charlie.toml
+
+j=0
 for i in alice bob charlie; do
 loc=parity/config/$i.toml
-sed -i '' -e  s,accountx,$$i,g $loc
 sed -i '' -e "/validators/s/^#//g" $loc
 sed -i '' -e "/signer/s/^#//g" $loc
 sed -i '' -e "/account/s/^#//g" $loc
