5603ce3968b69c175afeeb8e0df3e6a2110b2195
Sahal Zain <zean00zone@gmail.com>
2019-09-01 20:31:14 +0700

    adapt

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -A loc mm mmE mmlog
+declare -a loc mm mmE mmlog
 
 green=`tput setaf 2`
 reset=`tput sgr0`
@@ -11,7 +11,7 @@ rm -rf parity/config/secret/db.*
 rm -rf parity/config/db.*
 
 # get up to speed
-docker-compose pull ss1 alice
+#docker-compose pull ss1 alice
 
 # setup of 3 secret nodes
 
@@ -22,21 +22,23 @@ do
     cp parity/config/secret/ss$i.bak.toml ${loc[$i]}
 
     # create secret accounts
-    ss[$i]=$(docker run --rm -i -v $PWD/parity/config:/parity/config kryha/parity:secretstore --config ${loc[$i]} account new)
-
+    ss[$i]=$(docker run --rm -i -v $PWD/parity/config:/parity/config zean00/parity:ss --config /${loc[$i]} account new)
+    #echo ${ss[$i]}
     # cutting the 0x 
     ssx[$i]=$(echo ${ss[$i]}|cut -d "x" -f 2)
-    
-    # replacing dummy variables with accounts
-    sed -i '' -e  s,"accountx","${ssx[$i]}",g -e "/self_secret/s/^#//g" ${loc[$i]}
+    # echo ${ssx[$i]}
+    # # replacing dummy variables with accounts
+    gsed -i -e  s,"accountx","${ssx[$i]}",g -e "/self_secret/s/^#//g" ${loc[$i]}
 
-    # grabbing the enode and server public key from the logs
+    # # grabbing the enode and server public key from the logs
 
-    sslog[$i]=$(timeout 10s docker-compose up ss$i)
+    sslog[$i]=$(gtimeout 10s docker-compose up ss$i)
     ssp[$i]=$(echo "${sslog[$i]}"|grep "SecretStore node:"|cut -d "x" -f 2)
+    #echo ${ssp[$i]}
     ssE[$i]=$(echo "${sslog[$i]}"|grep "Public node URL:"|cut -d "/" -f 3)
-    docker kill $(docker ps -q)
-
+    #echo ${ssE[$i]}
+    #docker kill $(docker ps -q)
+    #docker-compose rm -f -s ss$i
 done
 
 # setup alice bob and charlie
@@ -48,12 +50,14 @@ do
     cp parity/config/$i.bak.toml ${loc[$i]}
     
     # create accounts
-    mm[$i]=$(docker run --rm -i -v $PWD/parity/config:/parity/config kryha/parity:beta --config ${loc[$i]} account new)
-
+    mm[$i]=$(docker run --rm -i -v $PWD/parity/config:/parity/config parity/parity:stable --config /${loc[$i]} account new)
+    echo ${mm[$i]}
     # grep enode
-    mmlog[$i]=$(timeout 10s docker-compose up $i)
+    mmlog[$i]=$(gtimeout 10s docker-compose up $i)
     mmE[$i]=$(echo "${mmlog[$i]}"|grep "Public node URL:"|cut -d "/" -f 3)
-    docker kill $(docker ps -q)
+    #echo ${mmE[$i]}
+    #docker kill $(docker ps -q)
+    #docker-compose rm -f -s $i
 
 done
 
@@ -61,7 +65,7 @@ done
 for i in alice bob charlie 1 2 3;
 do
     # accounts
-    sed -i ''   -e "/validators/s/^#//g"    -e "/signer/s/^#//g" \
+    gsed -i    -e "/validators/s/^#//g"    -e "/signer/s/^#//g" \
                 -e "/account/s/^#//g"       -e "/unlock/s/^#//g" \
                 -e "/bootnodes/s/^#//g"     -e "/nodes/s/^#//g" \
                 -e "s,accountx,${mm[$i]},g" ${loc[$i]}
@@ -71,25 +75,25 @@ do
     for j in alice bob charlie 1 2 3;
     do
         # enodes and public addresses
-            sed -i '' -e "s,mmE$j,${mmE[$j]},g" -e "s,ssE$j,${ssE[$j]},g" \
+            gsed -i  -e "s,mmE$j,${mmE[$j]},g" -e "s,ssE$j,${ssE[$j]},g" \
                       -e s,ssp$j,${ssp[$j]},g  ${loc[$i]}
 
     done
 done
 
-read -p "Do you want to deploy the acl permissioning contract? (RECOMMENDED) ${green}(y/n)${reset}? " CONT
-if [ "$CONT" = "y" ]; then
+# read -p "Do you want to deploy the acl permissioning contract? (RECOMMENDED) ${green}(y/n)${reset}? " CONT
+# if [ "$CONT" = "y" ]; then
     
-    # deploy acl
-    pass=$(cat parity/config/secret/alice.pwd)
-    bash acl.sh ${mm["alice"]} ${mm["bob"]} $pass
+#     # deploy acl
+#     pass=$(cat parity/config/secret/alice.pwd)
+#     bash acl.sh ${mm["alice"]} ${mm["bob"]} $pass
 
-    #private contract
-    read -p "Do you want to deploy the example private contract? ${green}(y/n)${reset}? " CONT
-    if [ "$CONT" = "y" ]; then
+#     #private contract
+#     read -p "Do you want to deploy the example private contract? ${green}(y/n)${reset}? " CONT
+#     if [ "$CONT" = "y" ]; then
 
-        bash private.sh ${mm["alice"]} ${mm["bob"]} $pass
-    else
-      echo -e "${green}Setup done!${reset}"
-    fi
-fi
+#         bash private.sh ${mm["alice"]} ${mm["bob"]} $pass
+#     else
+#       echo -e "${green}Setup done!${reset}"
+#     fi
+# fi
